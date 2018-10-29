e7f6ff9ffbfb9d5b03032be08af8461d78485e06
edwinyan <edwinyan@yeah.net>
2018-10-29 19:52:16 +0800

    add mian function

---
+++ Firmware/main/main.c
@@ -0,0 +1,150 @@
+#include "common.h"
+#include "bsp.h"
+#include "bsp_os.h"
+#include "led_drv.h"
+#include "tc_common.h"
+#include "rfm_drv.h"
+#include "rfm_common.h"
+#include "rx.h"
+#include "tx.h"
+#include "uart_drv.h"
+
+OS_MUTEX	TX_MUTEX;		//uart tx mutex
+OS_MUTEX	RX_MUTEX;		//uart rx mutex
+
+OS_MUTEX	FIFO_MUTEX;
+
+static FIFO_T stFiFo;
+
+/*----------------------------------------------------------------------------*/
+//macro and variables
+#define  APP_CFG_TASK_START_STK_SIZE                    256u
+#define  APP_CFG_TASK_START_PRIO                        2u
+
+#define  APP_TX_TASK_STK_SIZE                    256u
+#define  APP_TX_TASK_PRIO                        3u
+
+
+static  OS_TCB   app_task_start_tcb;
+static  CPU_STK  app_task_start_stk[APP_CFG_TASK_START_STK_SIZE];
+
+static  OS_TCB   app_tx_task_tcb;
+static  CPU_STK  app_tx_task_stk[APP_TX_TASK_STK_SIZE];
+
+/*----------------------------------------------------------------------------*/
+//local function
+STATIC void app_rfm_tx_task(void *p_arg)
+{
+//	OS_ERR      err;
+
+	(void)p_arg;
+	
+	MSG("Creating Application Tasks: %d\r\n",__FPU_USED);
+	
+	TXsetup();
+
+	while (DEF_TRUE) 
+    {   
+    	if(module_index == MODULE_TYPE_433M){
+			TXloop();
+    	}
+    }
+}
+
+STATIC void app_task_create(void)
+{
+
+	OS_ERR      err;
+#if 1	
+    OSTaskCreate((OS_TCB       *)&app_tx_task_tcb,              
+                 (CPU_CHAR     *)"App rmf tx Task",
+                 (OS_TASK_PTR   )app_rfm_tx_task, 
+                 (void         *)0,
+                 (OS_PRIO       )APP_TX_TASK_PRIO,
+                 (CPU_STK      *)&app_tx_task_stk[0],
+                 (CPU_STK_SIZE  )app_tx_task_stk[APP_TX_TASK_STK_SIZE / 10],
+                 (CPU_STK_SIZE  )APP_TX_TASK_STK_SIZE,
+                 (OS_MSG_QTY    )0,
+                 (OS_TICK       )0,
+                 (void         *)0,
+                 (OS_OPT        )(OS_OPT_TASK_STK_CHK | OS_OPT_TASK_STK_CLR),
+                 (OS_ERR       *)&err);
+#endif
+
+}
+
+STATIC void app_task_start(void *p_arg)
+{
+    OS_ERR      err;
+	//u32 i =0;
+
+
+   (void)p_arg;
+
+    BSP_Init();                                                 /* Initialize BSP functions                             */
+    CPU_Init();                                                 /* Initialize the uC/CPU services                       */
+    
+    BSP_Tick_Init();                                            /* Start Tick Initialization                            */
+    
+    Mem_Init();                                                 /* Initialize Memory Management Module                  */
+    Math_Init();                                                /* Initialize Mathematical Module                       */
+
+    BSP_Peripheral_Init();
+
+#if OS_CFG_STAT_TASK_EN > 0u
+    OSStatTaskCPUUsageInit(&err);                               /* Compute CPU capacity with no task running            */
+#endif
+
+#ifdef CPU_CFG_INT_DIS_MEAS_EN
+    CPU_IntDisMeasMaxCurReset();
+#endif
+    MSG("Creating Application Tasks: %d\r\n",__FPU_USED);
+	OSMutexCreate((OS_MUTEX*	)&FIFO_MUTEX,
+				  (CPU_CHAR*	)"UART FIFO_MUTEX",
+				  (OS_ERR*		)&err);
+	OSMutexCreate((OS_MUTEX*	)&TX_MUTEX,
+				  (CPU_CHAR*	)"UART TX_MUTEX",
+				  (OS_ERR*		)&err);
+	OSMutexCreate((OS_MUTEX*	)&RX_MUTEX,
+				  (CPU_CHAR*	)"UART RX_MUTEX",
+				  (OS_ERR*		)&err);
+
+    app_task_create();                                            
+
+    while (DEF_TRUE) 
+    {   
+        //tc_run_all();
+		OSTimeDlyHMSM(0, 0, 0, 50, OS_OPT_TIME_HMSM_STRICT, &err);
+    }
+}
+
+
+/*----------------------------------------------------------------------------*/
+int main(void)
+{
+    OS_ERR err;
+
+    //BSP_IntDisAll();
+    OSInit(&err);
+
+    OSTaskCreate((OS_TCB       *)&app_task_start_tcb,              
+                 (CPU_CHAR     *)"App Task Start",
+                 (OS_TASK_PTR   )app_task_start, 
+                 (void         *)0,
+                 (OS_PRIO       )APP_CFG_TASK_START_PRIO,
+                 (CPU_STK      *)&app_task_start_stk[0],
+                 (CPU_STK_SIZE  )app_task_start_stk[APP_CFG_TASK_START_STK_SIZE / 10],
+                 (CPU_STK_SIZE  )APP_CFG_TASK_START_STK_SIZE,
+                 (OS_MSG_QTY    )0,
+                 (OS_TICK       )0,
+                 (void         *)0,
+                 (OS_OPT        )(OS_OPT_TASK_STK_CHK | OS_OPT_TASK_STK_CLR),
+                 (OS_ERR       *)&err);
+
+    OSStart(&err);                                              
+    
+    (void)&err;
+    
+    return 0;
+}
+
