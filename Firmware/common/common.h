8081d71295459343ea2210d76e4418240a1d78a4
edwinyan <edwinyan@yeah.net>
2018-10-29 21:18:29 +0800

    add target common file

---
+++ Firmware/common/common.h
@@ -0,0 +1,31 @@
+#ifndef _COMMON_H_
+#define _COMMON_H_
+
+//generic
+#include  <stdio.h>
+#include  <stdarg.h>
+#include  <stdlib.h>
+#include  <string.h>
+#include  <math.h>
+
+//os relative
+#include  <cpu.h>
+#include  <cpu_core.h>
+#include  <os.h>
+#include  <lib_def.h>
+#include  <lib_ascii.h>
+#include  <lib_math.h>
+#include  <lib_mem.h>
+#include  <lib_str.h>
+
+//debug
+#include "debug_msg.h"
+
+//platform
+#include "stm32f4xx.h"
+
+
+
+//misc
+#define  STATIC     static
+#endif
